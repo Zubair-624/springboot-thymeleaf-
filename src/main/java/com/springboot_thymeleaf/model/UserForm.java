edbe2dc4f8c22b0047e26b9dd0f06430ea0aee2c
Zubair624 <zubairmazumder624@gmail.com>
2025-06-01 22:39:56 +0600

    add UserForm model for capturing user registration input

---
+++ src/main/java/com/springboot_thymeleaf/model/UserForm.java
@@ -0,0 +1,27 @@
+package com.springboot_thymeleaf.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserForm {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String gender;
+
+    private String address;
+
+    private boolean married;
+
+    private String profession;
+}
